bf1e69fbb7b84cdc0beccca16219c3acab5c2d0a
enrico-amerio <amerio.enrico95@gmail.com>
2024-03-08 16:46:56 +0100

    [fix]

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -16,15 +16,15 @@ for (let i = 0; i > 5; i++) {
 }
 
 /* 1 questo codice fa un log del valore di i per ogni ciclo 
-2 no
-3 si, se i è = a 0 la condizione i > 5 non si verificherà mai quindi il ciclo non partirà mai*/
+2 si, se i è = a 0 la condizione i > 5 non si verificherà mai quindi il ciclo non partirà mai
+3 no*/
 
 
 // ESERCIZIO 2
 num = 3;
 addIfEven(num)
 function addIfEven(num) {
-    if (num % 2 == 0) {
+    if (num % 2 === 0) {
         return num + 5;
     }
     return num;
@@ -32,10 +32,11 @@ function addIfEven(num) {
 console.log(addIfEven(num));
 /* 1 questo codice da un return se il numero è pari 
 2 si, 2 = 0 è sbagliato perchè assegna il valore invece di confrontare, dovrebbe essere 2 == 0 ( o 2 === 0)
-3 no */
+3 non viene mai richiamata quindi non parte */
 
 
 // ESERCIZIO 3
+loopToFive()
 function loopToFive() {
     for (let i = 0; i < 5; i++){
         console.log(i);
@@ -44,7 +45,7 @@ function loopToFive() {
 /*
 1. Questo loop genera un console log finchè il valore di i è minore di 5
 2. nel ciclo for vanno utilizzati i ; e non , 
-3.no
+3. non viene mai richiamata quindi non parte
 */
 
 
@@ -52,8 +53,8 @@ function loopToFive() {
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 let evenNumbers = [];
 function displayEvenNumbers() {
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] % 2 == 0){
+    for (let i = 0; i < numbers.length; i++){
+        if (numbers[i] % 2 === 0){
             evenNumbers.push(numbers[i]);
             console.log(numbers[i]);
         }
@@ -64,5 +65,6 @@ displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
 console.log(evenNumbers);
 /*
 1. questa funzione deve restituire un nuovo array contenente solo i numeri pari dell'array numbers
-2. c'è un ; dopo i++ // nella condizione dell'if c'è assegnazione e non confronto // tolgo il -1 nella condizione del ciclo se no non pusha l'8 // Il return interrompe il ciclo dopo il primo giro, va messo fuori dal ciclo alla fine della funzione // Tolgo il punto e virgola dopo alla tonda dell'if // dichiaro gli array fuori dalla funzione
+2. c'è un ; dopo i++ // nella condizione dell'if c'è assegnazione e non confronto // Il return interrompe il ciclo dopo il primo giro, va messo fuori dal ciclo alla fine della funzione // Tolgo il punto e virgola dopo alla tonda dell'if // dichiaro gli array fuori dalla funzione
+3.tolgo il -1 nella condizione del ciclo se no non pusha l'8 // messo numbers[i] nel push e nell'if
 */
